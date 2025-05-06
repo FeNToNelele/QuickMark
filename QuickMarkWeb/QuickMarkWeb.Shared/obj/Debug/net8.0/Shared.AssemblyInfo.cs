a2e5d10c77dc6eb52a6fae2ccbf7a23fd12cd381
FeNToNelele <sarosigab@gmail.com>
2025-05-06 12:42:24 +0200

    Upgraded project to Swagger v8.

--- QuickMarkWeb/QuickMarkWeb.Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ QuickMarkWeb/QuickMarkWeb.Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb965f9cf562040815125651974ea66585b63ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbe57c1423f885789e8149218077a91a46cbef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
